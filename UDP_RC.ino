d42060c8de2c293e41649de2f547c449a7fa26fc
Makerprojects <Glitzi85>
2016-07-03 09:48:30 +0200

    Prototyp for Maker Faire and Makerprojects Blog
    Minimal changes introduced to demonstrate concept feasibility.

--- UDP_RC.ino
+++ UDP_RC.ino
@@ -1,15 +1,39 @@
 /*
-UDP Controll LED.
-No change necessary.
+UDP Remote Control
+
+Please refer to www.pikoder.com for more information
+
+last change: 05.29.16 - initial release
+
+Copyright 2016 Gregor Schlechtriem
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
 */
 
 #define DEBUG true
 
-#define LED 9
+#define cNumChan 2
+
+#define Channel_1 9
+#define Channel_2 10
 
 #include <SoftwareSerial.h>
+#include <Servo.h>
 
 SoftwareSerial esp8266(11, 12); // RX, TX
+Servo myChan1;
+Servo myChan2;
 
 void setup() {
   Serial.begin(19200);
@@ -30,33 +54,30 @@ void setup() {
   //shorter Timeout for faster wrong UPD-Comands handling
     esp8266.setTimeout(1000);
    
-    pinMode(LED, OUTPUT);
-    
+    myChan1.attach(Channel_1);
+    myChan1.write(90);   // neutral position     
+
+    myChan2.attach(Channel_2);
+    myChan2.write(90);   // neutral position     
 }
 
 void loop() {
-  if (esp8266.available())
-  {
-    if (esp8266.find("+IPD,"))
-    {
-      if (esp8266.find("led")) {
-        int setLed = esp8266.parseInt();
-        digitalWrite(LED, setLed);
-
-        debug("LED=" + String(setLed));
-        if (sendCom("AT+CIPSEND=7", ">"))
-        {
-          sendCom("LED=" + String(setLed), "OK");
+  if (esp8266.available()){
+    if (esp8266.find("+IPD,6:")) {
+      delay(10);                          // make sure that all chars were received...
+      for (int i = 0; i < cNumChan; i++) {    
+        int Command = esp8266.read();
+        if (Command == 255) { 
+          int chanNo = esp8266.read();
+          int servoPos = esp8266.read();
+          debug("Command = " + String(Command)+ " Channel: " + String(chanNo)+ " ServoPos: " + String(servoPos));
+          servoPos = map(servoPos,0,254,45,135);  
+          if (chanNo == 0) { 
+            myChan1.write(servoPos);   // set to position
+          } else {
+            myChan2.write(servoPos);   // write actual position       
+          }
         }
-      }
-
-      else {
-        debug("Wrong UDP Command");
-        if (sendCom("AT+CIPSEND=19", ">"))
-        {
-          sendCom("Wrong UDP Command", "OK");
-        }
- 
       }
     }
   }
@@ -85,6 +106,18 @@ boolean configUDP()
 }
 
 //-----------------------------------------------Controll ESP-----------------------------------------------------
+
+boolean sendUDP(String Msg)
+{
+  boolean succes = true;
+
+  succes &= sendCom("AT+CIPSEND=" + String(Msg.length() + 2), ">");    //+",\"192.168.4.2\",90", ">");
+  if (succes)
+  {
+    succes &= sendCom(Msg, "OK");
+  }
+  return succes;
+}
 
 boolean sendCom(String command, char respond[])
 {
